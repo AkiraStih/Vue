0f58d4dd9ea75d1a968433d2831813a82a464d14
Bogdan <your_email@example.com>
2025-11-23 21:43:59 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Button from "./components/Button.vue";
+let  b = "Контент";
+let color = "red";
+</script>
+
+<template>
+  <header :class="color">Лого</header>
+  <main>{{b}}
+    <Button>Сохранить</Button>
+  </main>
+
+  <footer>Подвал</footer>
+</template>
+
+<style scoped>
+.blue{
+  color: aqua;
+}
+
+.red{
+  color: brown;
+}
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+  <button class="button">
+    <slot/>
+    </button>
+</template>
+
+<style scoped>
+  .button{
+    border: none;
+    border-radius: 10px;
+    background: var(--gradient);
+    padding: 14px;
+    font-family: var(--font);
+    font-size: 20px;
+    font-weight: 600;
+    color: var(--color-primary);
+    cursor: pointer;
+    transition: all 0.5s ease;
+  }
+  .button:hover{
+    filter: hue-rotate(-50deg);
+  }
+</style>
